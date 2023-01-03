bd55bef75a3320c7ee3e0dedd0afdc9a0935f494
nairansilva <nairan.asilva@gmail.com>
2023-01-03 10:23:55 -0300

    Adicionardo Auth Guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
 import { TurmasModule } from './turmas/turmas.module';
 
 @Module({
   imports: [TurmasModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/turmas/turmas.controller.ts
+++ src/turmas/turmas.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { TurmasService } from './turmas.service';
 import { CreateTurmaDto } from './dto/create-turma.dto';
 import { UpdateTurmaDto } from './dto/update-turma.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('turmas')
 export class TurmasController {
