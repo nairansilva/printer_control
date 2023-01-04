b7a131784c6146b9c1e09ba1db6ad99215112200
nairansilva <nairan.asilva@gmail.com>
2023-01-04 18:54:31 -0300

    Teste swagger publicado

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {
+  SwaggerModule.setup('api', app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
   });
 
