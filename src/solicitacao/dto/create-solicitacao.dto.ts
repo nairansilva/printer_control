75d12c256c3b646aac20540e22d122d8718ae278
nairansilva <nairan.asilva@gmail.com>
2023-01-11 15:00:17 -0300

    inclusão da propriedade status nas solicitações

--- src/solicitacao/dto/create-solicitacao.dto.ts
+++ src/solicitacao/dto/create-solicitacao.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber, IsBoolean, IsEmail } from 'class-validator';
+import { IsString, IsNumber, IsBoolean, IsEmail, isNumber } from 'class-validator';
 import { Solicitacao } from '@prisma/client';
 
 export class CreateSolicitacaoDto implements Solicitacao {
@@ -19,5 +19,8 @@ export class CreateSolicitacaoDto implements Solicitacao {
 
   @IsString()
   codTurma: string;
+
+  @isNumber()
+  status: number
 
 }
